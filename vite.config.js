fab04c1d81018c199901a641207f837631e73ef5
4crete4-debug <4crete4@gmail.com>
2026-06-19 18:31:44 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  build: {
+    chunkSizeWarningLimit: 5000,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  }
 });
